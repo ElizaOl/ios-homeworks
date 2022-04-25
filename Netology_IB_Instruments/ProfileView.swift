fb4e3d6da109970b78cb67a0a63f219af4b9fb6b
ElizaOL <lizaolubowale@gmail.com>
2022-04-25 09:27:17 +0300

    homework 2

---
+++ Netology_IB_Instruments/ProfileView.swift
@@ -0,0 +1,17 @@
+//
+//  ProfileView.swift
+//  Netology_IB_Instruments
+//
+//  Created by Elizabeth Olubowale on 25.04.2022.
+//
+
+import UIKit
+
+class ProfileView: UIView {
+
+    @IBOutlet var profileImage: UIImageView!
+    @IBOutlet var username: UILabel!
+    @IBOutlet var birthday: UILabel!
+    @IBOutlet var location: UILabel!
+    
+}
