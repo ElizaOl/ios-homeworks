27531888da25ab11452e9cd387de0f01ec0ff36e
ElizaOL <lizaolubowale@gmail.com>
2022-05-09 19:47:48 +0300

    homework 2.1

---
+++ Lesson_4_Layout/Lesson_4_Layout/Profile/ProfileViewController.swift
@@ -0,0 +1,8 @@
+//
+//  ProfileViewController.swift
+//  Lesson_4_Layout
+//
+//  Created by Elizabeth Olubowale on 02.05.2022.
+//
+
+import Foundation
---
+++ Lesson_4_Layout/Lesson_4_Layout/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lesson_4_Layout
+//
+//  Created by Elizabeth Olubowale on 02.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
